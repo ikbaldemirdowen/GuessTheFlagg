f2b89f800399aed6d899aaa387a356eb996a61b2
ikbal demirdoven <ikbaldemirdoven@icloud.com>
2022-07-18 02:14:07 -0700

    final version

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -10,11 +10,12 @@ import SwiftUI
 struct ContentView: View {
     @State private var showingScore = false
     @State private var scoreTitle = ""
-    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
-    @State private var correctAnswer = Int.random(in: 0...4)
+    static let allCountries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    @State private var countries = allCountries.shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
     @State private var score = 0
-    @State private var dismiss = false
-
+    @State private var questionCounter = 1
+    @State private var showingResults = false
     var body: some View {
         ZStack
         {
@@ -46,8 +47,10 @@ struct ContentView: View {
                             .bold()
                     }
                 }
+                
                 .padding()
                 .padding()
+                
                 VStack
                 {
                     Text("Tag the flag of")
@@ -56,20 +59,27 @@ struct ContentView: View {
                     Text("\(countries[correctAnswer])")
                         .foregroundColor(.black)
                         .font(.largeTitle.weight(.semibold))
-                }
+                
             
-                ForEach(0..<5)
-                {
-                    number in
-                    Button
+                    ForEach(0..<3)
                     {
-                        flagTapped(number)
-                    } label: {
-                        Image(countries[number])
-                            .clipShape(Capsule())
-                            .shadow(radius: 100)
+                        number in
+                        Button
+                        {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .clipShape(Capsule())
+                                .shadow(radius: 100)
+                                .padding(10)
+                        }
                     }
                 }
+                .padding(35)
+                .frame(maxWidth: .infinity)
+                .background(.ultraThinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .padding(25)
                 Spacer()
                 VStack
                 {
@@ -77,20 +87,31 @@ struct ContentView: View {
                     {
                         askQuestion()
                         score = 0
+                        questionCounter = 0
+                        showingResults = false
+                        showingScore = false
+                        countries = Self.allCountries.shuffled()
+                        correctAnswer = Int.random(in: 0...2)
                     }
                     .foregroundColor(.black)
                 }
+                
                 Spacer()
+                
             }
-//            .frame(maxWidth: .infinity)
-//            .background(.ultraThinMaterial)
-//            .clipShape(RoundedRectangle(cornerRadius: 20))
+            
         }
         .alert(scoreTitle, isPresented: $showingScore)
         {
             Button("Continue", action: askQuestion)
         } message: {
             Text("Your score is \(score) now.")
+        }
+        .alert("Game Over!", isPresented: $showingResults)
+        {
+            Button("Start Again", action: newGame)
+        } message: {
+            Text("Your final score was \(score).")
         }
     }
     func flagTapped(_ number : Int)
@@ -102,16 +123,36 @@ struct ContentView: View {
         }
         else
         {
-            scoreTitle = "Wrong!"
-            score -= 1
+            scoreTitle = "Wrong! That's the flag of \(countries[number])"
+            if score > 0
+            {
+                score -= 1
+            }
         }
-        showingScore = true
+        if questionCounter == 8
+        {
+            showingResults = true
+        }
+        else
+        {
+            showingScore = true
+        }
     }
     
     func askQuestion()
     {
+        countries.remove(at: correctAnswer)
         countries.shuffle()
-        correctAnswer = Int.random(in: 0...4)
+        correctAnswer = Int.random(in: 0...2)
+        questionCounter += 1
+    }
+    
+    func newGame()
+    {
+        questionCounter = 0
+        score = 0
+        countries = Self.allCountries
+        askQuestion()
     }
 }
     
